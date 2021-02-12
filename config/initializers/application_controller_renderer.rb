abfb8516d0bebebb052361aec19b5ba2a3ebd909
abi0116 <kosukeabiru.www@gmail.com>
2021-02-12 02:22:51 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'cd0822f9950967c48eb66b896914729d3573afdb493f8b658767a2aad6f6cb2a934ae08aea5c4457a27f38a6229b547b15e218d19302450cdbe81a1c085a7756'
